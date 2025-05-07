ce13eb2a926cfeac2b14d3c1f1e4b68b812d5def
dax-dot-gay <matteovh@gmail.com>
2025-05-07 14:34:33 -0400

    start setting up c# sidecar

---
+++ aextract/AextractSidecar/Program.cs
@@ -0,0 +1,10 @@
+﻿static class AExtract
+{
+    static void Main(string[] args)
+    {
+        string Requests = args[0];
+        string TargetDirectory = args[1];
+
+        DirectoryInfo output_dir = System.IO.Directory.CreateDirectory(Path.Join(TargetDirectory, "assets"));
+    }
+}

5104fdfc4477a4726ad4ee1d6af022ae2083a7b4
dax-dot-gay <matteovh@gmail.com>
2025-05-09 11:42:59 -0400

    setup tauri app

---
+++ orbital/src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    #[cfg(debug_assertions)] // only enable instrumentation in development builds
+    let devtools = tauri_plugin_devtools::init();
+
+    let mut builder = tauri::Builder::default();
+    builder = builder
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet]);
+
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+
+    builder
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
